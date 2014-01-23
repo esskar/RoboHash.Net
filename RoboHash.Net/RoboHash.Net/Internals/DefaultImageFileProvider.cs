beeff4d0933c9a4da281a7e83dec28bd07713d5d
esskar <sk@intertivity.com>
2014-01-23 12:52:27 +0100

    fix file and folder caseing

---
+++ RoboHash.Net/RoboHash.Net/Internals/DefaultImageFileProvider.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using RoboHash.Net.Interfaces;
+
+namespace RoboHash.Net.Internals
+{
+    internal class DefaultImageFileProvider : IRoboHashImageFileProvider
+    {
+        private static readonly string[] _sets;
+        private static readonly string[] _backgroundSets;
+        private static readonly string[] _colors;
+        private static readonly string _resourcePath;
+
+        static DefaultImageFileProvider()
+        {
+            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+
+            _resourcePath = Path.GetDirectoryName(assembly.Location);
+            if (_resourcePath == null)
+                throw new InvalidOperationException("Failed to retrieve resource path.");
+
+            _sets = GetDirectoryNames(Path.Combine(_resourcePath, RoboConsts.SetsDir)).ToArray();
+            _backgroundSets = GetDirectoryNames(Path.Combine(_resourcePath, RoboConsts.BackgroundsDir)).ToArray();
+            _colors = GetDirectoryNames(Path.Combine(_resourcePath, RoboConsts.SetsDir, _sets[0])).ToArray();
+        }
+
+        public string[] Sets
+        {
+            get { return _sets; }
+        }
+
+        public string[] BackgroundSets
+        {
+            get { return _backgroundSets; }
+        }
+
+        public string[] Colors
+        {
+            get { return _colors; }
+        }
+
+        public string BasePath
+        {
+            get { return _resourcePath; }
+        }
+
+        public IEnumerable<string> GetDirectories(string path)
+        {
+            return Directory.EnumerateDirectories(path);
+        }
+
+        public IList<string> GetFiles(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        private static IEnumerable<string> GetDirectoryNames(string path)
+        {
+            foreach (var iterDir in Directory.EnumerateDirectories(path))
+            {
+                var dirPath = iterDir;
+                if (dirPath.StartsWith(path))
+                {
+                    dirPath = dirPath.Substring(path.Length);
+                    dirPath = dirPath.TrimStart('\\', '/');
+                }
+                yield return dirPath;
+            }
+        }
+    }
+}
---
+++ RoboHash.Net/RoboHash.Net/RoboHash.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Text;
+using RoboHash.Net.Interfaces;
+using RoboHash.Net.Internals;
+
+namespace RoboHash.Net
+{
+    public class RoboHash : RoboHashBase<Image>
+    {
+        private static readonly IRoboHashImageFileProvider _imageFileProvider;
+        private static readonly IRoboHashDigestGenerator _digestGenerator;
+
+        static RoboHash()
+        {
+            _imageFileProvider = new DefaultImageFileProvider();
+            _digestGenerator = new DefaultDigestGenerator();
+        }
+
+        /// <summary>
+        /// Creates a robohash from the given text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        public static RoboHash Create(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return Create(bytes, 0, bytes.Length);
+        }
+
+        /// <summary>
+        /// Creates a robohash from the given byte array.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <param name="offset">The offset.</param>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        public static RoboHash Create(byte[] bytes, int offset, int length)
+        {
+            using (var memory = new MemoryStream(bytes, offset, length))
+                return Create(memory);
+        }
+
+        /// <summary>
+        /// Creates a robohash from the specified stream.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <returns></returns>
+        public static RoboHash Create(Stream stream)
+        {
+            var hexDigest = _digestGenerator.GenerateHexDigest(stream);
+            return new RoboHash(hexDigest, _imageFileProvider);
+        }
+
+        public RoboHash(string hexDigest, IRoboHashImageFileProvider imageFileProvider)
+            : base(hexDigest, imageFileProvider) { }
+
+
+        protected override Image RenderFiles(IEnumerable<string> srcFiles, int srcWidth, int srcHeight, int destWidth, int destHeight)
+        {
+            var retval = new Bitmap(srcWidth, srcHeight);
+            using (var canvas = Graphics.FromImage(retval))
+            {
+                canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                foreach (var imageFile in srcFiles)
+                {
+                    using (var image = Image.FromFile(imageFile))
+                        canvas.DrawImage(image, new Rectangle(0, 0, srcWidth, srcHeight),
+                            new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+                }                
+                canvas.Save();
+            }
+
+            if (srcWidth != destWidth || srcHeight != destHeight)
+            {
+                var resizedImage = new Bitmap(retval, destWidth, destHeight);
+                retval.Dispose();
+
+                retval = resizedImage;
+            }
+            
+            return retval;
+        }
+    }
+}
